466e1961d8c880463ce2ce814dd44e060d16fbde
Harsh4999 <trivediharsh49@gmail.com>
2022-03-21 15:29:40 +0530

    improved code

--- amazon.js
+++ amazon.js
@@ -4,8 +4,9 @@ const cronjob = require("cron").CronJob;
 const nodemailer = require("nodemailer");
 const url =
   "https://www.amazon.in/s?k=shoe&crid=Y9U1OHNHT8IM&sprefix=shoe%2Caps%2C279&ref=nb_sb_noss_2";
+ let browser; 
 async function getData() {
-  const browser = await puppeteer.launch();
+ browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.setViewport({ width: 1600, height: 900 });
   await page.goto(url, { waitUntil: "networkidle0" });
@@ -14,35 +15,46 @@ async function getData() {
 async function checkPrice(page) {
   await page.reload({ waitUntil: "networkidle0" });
   let html = await page.evaluate(() => {
-    return document.body.getElementsByClassName('s-main-slot s-result-list s-search-results sg-row')[0].innerHTML;
+    return document.getElementsByClassName('s-main-slot s-result-list s-search-results sg-row')[0].children
     //console.log(document.body.getElementsByClassName('s-main-slot s-result-list s-search-results sg-row')[0].innerHTML)
   });
-  const $ = cheerio.load(html);
-  // const price=[];
-  // $(".a-price-whole", html).each(function () {
-  //   let pricegandu = $(this).text();
-  //   price.push(pricegandu);
-  // });
-  // console.log(price.length);
+  console.log(typeof html);
+  for(x of html){
+    console.log(x);
+  }
+//  const $ = cheerio.load(html);
 
-  const products=[];
-  $(".a-size-base-plus", html).each(function () {
-	let name = $(this).text();
-	products.push(name);
-	
-      });
-	console.log(products);
- 
+//   const price=[];
+//   $(".a-price-whole", html).each(function () {
+//     let pricegandu = $(this).text();
+//     price.push(pricegandu);
+//   });
+//   console.log(price.length);
+
+//   const products=[];
+//   $("h5 .a-size-base-plus", html).each(function () {
+// 	let name = $(this).text();
+// 	 products.push(name);
+//   });
+// 	console.log(products.length);
+//   const productDesciption = [];
+//   $('h2 .a-size-base-plus',html).each(function (){
+//     let desc = $(this).text();
+//     productDesciption.push(desc);
+//   })
+//  console.log('product description = ',productDesciption.length);
 // const img=[];
 //   $(".s-image", html).each(function () {
 //     let imageurl = $(this).attr('src');
 //     img.push(imageurl);
 //   });
-//   console.log(img)
+//   console.log(img.length)
+
  }
+
 async function main() {
   const page = await getData();
   await checkPrice(page);
-  // await browser.close();
+  await browser.close();
 }
 main();
