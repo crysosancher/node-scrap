26d2fe91e107eee0defc76131c25286b48e30807
crysosancher <vibhupandey.vp@gmail.com>
2022-03-21 22:41:51 +0530

    added +2

--- amazon.js
+++ amazon.js
@@ -3,9 +3,11 @@ const cheerio = require("cheerio");
 const cronjob = require("cron").CronJob;
 const nodemailer = require("nodemailer");
 const url =
-  "https://www.amazon.in/s?k=shoe&crid=Y9U1OHNHT8IM&sprefix=shoe%2Caps%2C279&ref=nb_sb_noss_2";
+  "https://www.amazon.in/s?k=sindoor&crid=Y9U1OHNHT8IM&sprefix=sindoor%2Caps%2C279&ref=nb_sb_noss_2";
+  //"https://www.amazon.in/s?k=watch&crid=3L3TRTINOKLXF&sprefix=watch%2Caps%2C289&ref=nb_sb_noss";
+  let browser;
 async function getData() {
-  const browser = await puppeteer.launch();
+  browser = await puppeteer.launch({headless: false});
   const page = await browser.newPage();
   await page.setViewport({ width: 1600, height: 900 });
   await page.goto(url, { waitUntil: "networkidle0" });
@@ -18,20 +20,26 @@ async function checkPrice(page) {
     //console.log(document.body.getElementsByClassName('s-main-slot s-result-list s-search-results sg-row')[0].innerHTML)
   });
   const $ = cheerio.load(html);
-  // const price=[];
-  // $(".a-price-whole", html).each(function () {
-  //   let pricegandu = $(this).text();
-  //   price.push(pricegandu);
-  // });
+  const price=[];
+  $(".a-price-whole", html).each(function () {
+    let pricegandu = $(this).text();
+    price.push(pricegandu);
+  });
   // console.log(price.length);
 
   const products=[];
-  $(".a-size-base-plus", html).each(function () {
+  $("h2 .a-size-base-plus", html).each(function () {
 	let name = $(this).text();
 	products.push(name);
 	
       });
-	console.log(products);
+  const brand=[];
+  $("h5 .a-size-base-plus", html).each(function () {
+	let Bname = $(this).text();
+	brand.push(Bname);
+	
+      });
+	//console.log("Brand=>",brand,products);
  
 // const img=[];
 //   $(".s-image", html).each(function () {
@@ -39,10 +47,14 @@ async function checkPrice(page) {
 //     img.push(imageurl);
 //   });
 //   console.log(img)
+console.log("brand=",brand.length,"Product Name=",products.length,"Price=",price.length);
+for(let i=0;i<price.length;i++){
+    console.log("Product Name=>",products[i],"Price=>",price[i+3]);
+}
  }
 async function main() {
   const page = await getData();
   await checkPrice(page);
-  // await browser.close();
+  //await browser.close();
 }
 main();
