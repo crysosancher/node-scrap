c3b57672dfa30f57df815a8dafdfc70e79868f7f
crysosancher <vibhupandey.vp@gmail.com>
2022-03-21 23:18:42 +0530

    addded link

---
+++ smashVirat.js
@@ -0,0 +1,4 @@
+//football:-sg-col-4-of-12 s-result-item s-asin sg-col-4-of-16 AdHolder sg-col s-widget-spacing-small sg-col-4-of-20
+//mobile:s-result-item s-asin sg-col-0-of-12 sg-col-16-of-20 AdHolder sg-col s-widget-spacing-small sg-col-12-of-16
+//condom:-sg-col-4-of-12 s-result-item s-asin sg-col-4-of-16 AdHolder sg-col s-widget-spacing-small sg-col-4-of-20
+//condom dotted:-sg-col-4-of-12 s-result-item s-asin sg-col-4-of-16 AdHolder sg-col s-widget-spacing-small sg-col-4-of-20
