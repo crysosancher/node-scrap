a8f05bd256fd1a1c049ce28117f2a871e4866d93
crysosancher <vibhupandey.vp@gmail.com>
2022-03-27 20:12:46 +0530

    Sd Working code for single page

--- puppy.js
+++ puppy.js
@@ -1,11 +1,12 @@
 const puppeteer = require("puppeteer");
 const cheerio = require("cheerio");
+var fs=require('fs');
 const url =
   "https://www.amazon.in/s?k=honey";
   let browser;
   const shelves = [];
   async function getData() {
-    browser = await puppeteer.launch({headless: false});
+    browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.setViewport({ width: 1600, height: 900 });
     await page.goto(url, { waitUntil: "networkidle0" });
@@ -14,12 +15,16 @@ const url =
   async function checkPrice(page) {
     await page.reload({ waitUntil: "networkidle0" });
     let html = await page.evaluate(() => {
-      return document.body.getElementsByClassName('s-main-slot s-result-list s-search-results sg-row')[0].innerHTML;
+      return document.body.getElementsByClassName('s-main-slot s-result-list s-search-results sg-row')[0].childNodes[5];
       //console.log(document.body.getElementsByClassName('s-main-slot s-result-list s-search-results sg-row')[0].innerHTML)
     });
+    console.log(html);
+    fs.writeFileSync('puppy.html', html);
     const $ = cheerio.load(html);
-    $('div.sg-col-4-of-12.s-result-item.s-asin.sg-col-4-of-16.sg-col.sg-col-4-of-20').each((_idx, el) => {
+    console.log("hey")
+    $("*").each((_idx, el) => {
 	const shelf = $(el)
+  console.log("shelf=",shelf)
 	const title = shelf.find('span.a-size-base-plus.a-color-base.a-text-normal').text()
 	const price = shelf.find('span.a-price-whole').text()
 	const imgUrl = shelf.find('img.s-image').attr('src')
@@ -30,7 +35,7 @@ const url =
 async function main() {
 	const page=await getData()
 	await checkPrice(page);
-	await console.log(shelves);
+	//await console.log(shelves);
 	await browser.close();
 }
 main();
---
+++ sd.js
@@ -0,0 +1,59 @@
+import axios from "axios";
+import * as cheerio from "cheerio";;
+import fs from "fs";
+ 
+const getData = async (searchTerm) => {
+  // Send GET Request
+  const response = await (
+    await axios.get(`https://www.amazon.in/s?k=${searchTerm}`, {
+      headers: {
+        "User-Agent":
+          "Mozilla/5.0 (Linux; Android 5.1; AFTS Build/LMY47O) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/41.99900.2250.0242 Safari/537.36",
+      },
+    })
+  ).data;
+  // Load the returned response
+  let $ = cheerio.load(response);
+  // Find Title Section of the page
+  let titleObj = $(
+    "h2 a.a-link-normal.a-text-normal",
+    'div[data-component-type="s-search-result"]'
+  );
+  // Find Price Section of the page
+  let priceObj = $(
+    "span.a-price:nth-of-type(1) span.a-offscreen",
+    'div[data-component-type="s-search-result"]'
+  );
+  let img = $(
+    `img.s-image`,
+    'div[data-component-type="s-search-result"]'
+  );
+  let pageLength=$(`.s-pagination-strip`,`div[role="navigation"]`);
+  //console.log("page length =",pageLength.length);
+  console.log("page length =",pageLength[0].children[5].children[0]['data']);
+  let data = [];
+  // Get all product names and links
+  for (let i = 0; i < titleObj.length; i++) {
+    let productName = $(titleObj[i]).find("span")[0].children[0]["data"];
+    let productLink = "https://amazon.in" + $(titleObj[i]).attr("href");
+    let price = $(priceObj[i]).text();
+    let imgUrl=$(img[i]).attr("src");
+    data.push({
+      productName: productName,
+      price: price,
+      imageUrl: imgUrl,
+      productLink: productLink,
+    });
+  }
+  //console.log(data);
+  return data;
+};
+
+async function main(){
+  let myData=await getData("headphones");
+  console.log(myData);
+  let jsonData=JSON.stringify(myData);
+ // fs.writeFileSync("data.json",jsonData);
+  //console.log(jsonData);
+}
+main();
