7571e36e6a92a5c1147251245f1d7f2d2e3c602d
crysosancher <vibhupandey.vp@gmail.com>
2022-03-28 12:07:38 +0530

    only name left

--- sd.js
+++ sd.js
@@ -1,12 +1,16 @@
 import axios from "axios";
 import * as cheerio from "cheerio";;
 import fs from "fs";
+import { format } from "path";
+let $;
+let pageLength;
+let data = [];
  
-const getData = async (searchTerm) => {
+const getData = async (searchTerm,pageNo) => {
   // Send GET Request
   //https://www.amazon.in/s?k=headphones&page=2
   const response = await (
-    await axios.get(`https://www.amazon.in/s?k=${searchTerm}&page=1`, {
+    await axios.get(`https://www.amazon.in/s?k=${searchTerm}&page=${pageNo||1}`, {
       headers: {
         "User-Agent":
           "Mozilla/5.0 (Linux; Android 5.1; AFTS Build/LMY47O) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/41.99900.2250.0242 Safari/537.36",
@@ -14,7 +18,14 @@ const getData = async (searchTerm) => {
     })
   ).data;
   // Load the returned response
-  let $ = cheerio.load(response);
+  $ = cheerio.load(response);
+};
+const getPageDetails = async () => {
+  let pageScrap=$(`.s-pagination-strip`,`div[role="navigation"]`);
+  //console.log("page length =",pageScrap[0].children[5].children[0]['data']);
+  pageLength=pageScrap[0].children[5].children[0]['data'];
+}
+const getProductDetails = async () => {
   // Find Title Section of the page
   let titleObj = $(
     "h2 a.a-link-normal.a-text-normal",
@@ -29,10 +40,7 @@ const getData = async (searchTerm) => {
     `img.s-image`,
     'div[data-component-type="s-search-result"]'
   );
-  let pageLength=$(`.s-pagination-strip`,`div[role="navigation"]`);
-  //console.log("page length =",pageLength.length);
-  console.log("page length =",pageLength[0].children[5].children[0]['data']);
-  let data = [];
+  
   // Get all product names and links
   for (let i = 0; i < titleObj.length; i++) {
     let productName = $(titleObj[i]).find("span")[0].children[0]["data"];
@@ -51,10 +59,18 @@ const getData = async (searchTerm) => {
 };
 
 async function main(){
-  let myData=await getData("headphones");
-  console.log(myData);
-  let jsonData=JSON.stringify(myData);
- // fs.writeFileSync("data.json",jsonData);
+  await getData("headphones");
+  getPageDetails();
+  getProductDetails();
+  console.log("page length =",pageLength);
+   for(let i=2;i<=pageLength;i++){
+    await getData("shoe",i);
+    getProductDetails();
+
+   }
+   console.log(data);
+  let jsonData=JSON.stringify(data);
+ fs.writeFileSync(`data.json`,jsonData);
   //console.log(jsonData);
 }
 main();
